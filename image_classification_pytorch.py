bb8cfb213efceb34d3db73e229f1d9efa80a6ce9
Sapir Benzino <63209732+Sapir52@users.noreply.github.com>
2022-03-03 11:45:44 +0200

    Add files via upload

---
+++ image_classification_pytorch.py
@@ -0,0 +1,301 @@
+# Project part 3
+# Imports
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.datasets import CIFAR100
+from torchvision.transforms import ToTensor
+from torchvision.utils import make_grid
+from torch.utils.data import random_split
+get_ipython().run_line_magic('matplotlib', 'inline')
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+from torch.utils.data.dataloader import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch.autograd import Variable
+from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout
+from torch.optim import Adam, SGD
+
+
+#Exploring the Data
+dataset = CIFAR100(root='data/', download=True, transform=ToTensor())
+test_dataset = CIFAR100(root='data/', train=False, transform=ToTensor())
+img, label = dataset[0]
+print(img)
+print(dataset.classes)
+
+def show_example(img, label):
+    print('Label: ', dataset.classes[label], "("+str(label)+")")
+    plt.imshow(img.permute(1, 2, 0))
+
+show_example(*dataset[0])
+
+#Training & Validation set
+val_size = 5000
+train_size = len(dataset) - val_size
+train_ds, val_ds = random_split(dataset, [train_size, val_size])
+print(len(train_ds), len(val_ds))
+batch_size=128
+train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+val_dl = DataLoader(val_ds, batch_size*2, num_workers=4, pin_memory=True)
+
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        break
+
+show_batch(train_dl)
+
+#--------------------------------------------------------------------------
+#Define CNN Model
+#convolution operation on a 1 channel image with a 3x3 kernel.
+def apply_kernel(image, kernel):
+    ri, ci = image.shape       # image dimensions
+    rk, ck = kernel.shape      # kernel dimensions
+    ro, co = ri-rk+1, ci-ck+1  # output dimensions
+    output = torch.zeros([ro, co])
+    for i in range(ro): 
+        for j in range(co):
+            output[i,j] = torch.sum(image[i:i+rk,j:j+ck] * kernel)
+    return output
+
+#define the model by extending an ImageClassificationBase class with helper methods for training & validation.
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+
+#--------------------------------------------------------------------------
+class Cifar100Cnn(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 64 x 16 x 16
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 128 x 8 x 8
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 256 x 4 x 4
+
+            nn.Flatten(), 
+            nn.Linear(256*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 100))
+        
+    def forward(self, xb):
+        return self.network(xb)
+
+model = Cifar100Cnn()
+print(model)
+
+#--------------------------------------------------------------------------
+def conv_block(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1), 
+              nn.BatchNorm2d(out_channels), 
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_classes):
+        super().__init__()
+        
+        self.conv1 = conv_block(in_channels, 64) # 3 x 32 x 32 -> 64 x 32 x 32
+        self.conv2 = conv_block(64, 128, pool=True) # 64 x 32 x 32 -> 128 x 16 x 16
+        self.res1 = nn.Sequential(conv_block(128, 128), conv_block(128, 128)) # 128 x 16 x 16 -> 128 x 16 x 16
+        
+        self.conv3 = conv_block(128, 256, pool=True) # 128 x 16 x 16 -> 256 x 8 x 8
+        self.conv4 = conv_block(256, 512, pool=True) # 256 x 8 x 8 -> 512 x 4 x 4
+        self.res2 = nn.Sequential(conv_block(512, 512), conv_block(512, 512)) # 512 x 4 x 4 -> 512 x 4 x 4
+        self.conv5 = conv_block(512, 1028, pool=True) # 512 x 4 x 4 -> 1028 x 2 x 2
+        self.res3 = nn.Sequential(conv_block(1028, 1028), conv_block(1028, 1028))  # 1028 x 2 x 2 -> 1028 x 2 x 2
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(2), # 1028 x 1 x 1
+                                        nn.Flatten(), # 1028 
+                                        nn.Linear(1028, num_classes)) # 1028 -> 100
+        
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.conv5(out)
+        out = self.res3(out) + out
+        out = self.classifier(out)
+        return out
+    
+model = ResNet9(3, 100)
+print(model)
+#--------------------------------------------------------------------------
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+    
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+device = get_default_device()
+device
+#--------------------------------------------------------------------------
+
+#Training The model
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase 
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+#model = to_device(Cifar10CnnModel(), device)
+model = to_device(ResNet9(3, 100), device)
+print(evaluate(model, val_dl))
+#Hyper parameters for training model
+num_epochs = 10
+opt_func = torch.optim.Adam
+lr = 0.001
+history = fit(num_epochs, lr, model, train_dl, val_dl, opt_func)
+
+def plot_accuracies(history):
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs');
+
+plot_accuracies(history)
+
+def plot_losses(history):
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs');
+
+plot_losses(history)
+
+#Test with individual images
+def predict_image(img, model):
+    # Convert to a batch of 1
+    xb = to_device(img.unsqueeze(0), device)
+    # Get predictions from model
+    yb = model(xb)
+    # Pick index with highest probability
+    _, preds  = torch.max(yb, dim=1)
+    # Retrieve the class label
+    return dataset.classes[preds[0].item()]
+
+img, label = test_dataset[0]
+plt.imshow(img.permute(1, 2, 0))
+print('Label:', dataset.classes[label], ', Predicted:', predict_image(img, model))
+
+test_loader = DeviceDataLoader(DataLoader(test_dataset, batch_size*2), device)
+result = evaluate(model, test_loader)
+# Saving and Loading the model
+torch.save(model.state_dict(), 'ResNet9_torch_cnn.pth')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
